50f21529b26246ad70a29ff6477be3fdd4581e9f
Ihor <ihorfront88@gmail.com>
2023-03-28 15:56:33 +0300

    add: 2 step

---
+++ db/contacts.js
@@ -0,0 +1,47 @@
+const { v4: uuidv4 } = require("uuid");
+
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+};
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const newContact = contacts.filter((_, index) => index !== idx);
+  await fs.writeFile(contactsPath, JSON.stringify(newContact));
+  return contacts[idx];
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: uuidv4() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
